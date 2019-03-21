fb3da4c7ff6775704df29ef3b73147f9bba8b5fb
mr7 <596786272@qq.com>
2019-03-21 16:00:09 +0800

    优化加载模块和约束

--- app.py
+++ app.py
@@ -3,14 +3,25 @@ from cmdb.types import get_instance
 
 jsonstr = """
 {
-    "type":"cmdb.types.IP",
-    "value":"192.168.0.1"
+    "type":"cmdb.types.Int",
+    "value":80,
+    "nullable":true,
+    "unique":false,
+    "option":{
+        "max":100,
+        "min":1
+    },
+    "multi":true
+    "reference":{
+        "schema":1,
+        "field":1,
+        "on_delete":cascade|set_null|disbale,
+        "on_update":cascade|disbale,
+    }
 }
 """
 obj = json.loads(jsonstr)
-a = get_instance(obj['type']).stringify(obj['value'])
+a = get_instance(obj['type'],**obj.get('option',{})).stringify(obj['value'])
 print(a)
-
-
 
 
--- cmdb/types/__init__.py
+++ cmdb/types/__init__.py
@@ -1,19 +1,71 @@
 import importlib
 import ipaddress  # 3.3版本才有
 
+# 类和实例的缓存字典
+clsaaes_cache = {}
+instance_cache = {}
 
-def get_instance(type: str):
-    '''插件化实现，需要的时候就动态加载'''
-    m, c = type.rsplit('.', maxsplit=1)
-    mod = importlib.import_module(m)
-    cls = getattr(mod, c)
-    obj = cls()
-    if isinstance(obj, BaseType):
+
+def get_class(type: str):
+    type = type.strip()
+    cls = clsaaes_cache.get(type)
+    if cls:
+        return cls
+
+    # m, c = type.rsplit('.', maxsplit=1)
+    # # '''插件化实现，需要的时候就动态加载，不会重复加载'''
+    # # mod = importlib.import_module(m)
+    # cls = getattr(mod, c)
+    #
+    # globals().get(type)
+    #
+    # clsaaes_cache[type] = cls
+    #
+    # if issubclass(cls, BaseType):
+    #     return cls
+    # raise TypeError(f'Wrong Type! {cls} is not sub class of {BaseType}.')
+
+
+#"cmdb.types.Int"  "Int" 两种名称
+def get_instance(type: str, **option):
+
+
+    option_key = ",".join("{}={}".format(k, v) for k, v in sorted(option.items()))
+    key = f"{type}:{option_key}"
+    # 如验证规则option改变，所以不同的规则，实例行为不同，所以要把规则加进去
+    '''
+    解决频繁创建实例的事情，创建越少，垃圾回收就越少，否则要清理大量的垃圾，垃圾清理的性能就越差
+    要清理调整，是因为有很多内存碎片都是不想要的，拿走就变成了内存空洞，这就要处理规整了
+    规整之后，就会有连续的大内存了，不然这些连续的内存都是千疮百孔
+    '''
+    obj = instance_cache.get(key)
+    if obj:
         return obj
-    raise TypeError(f'Wrong Type! {obj} is not sub class of {BaseType}.')
 
+    obj = get_class(type)(**option)
+    instance_cache[key] = obj
+    return obj
+
+'''
+因为在导入模块的函数的时候，模块已经加载了，所以可以直接通过globals()获取当前模块的名词空间的属性字典
+然后通过一些条件，把用户所需的类获取到，无论是长名称还是短名称
+'''
+def inject():
+    for n,t in globals().items():
+        if type(t)== type and issubclass(t,BaseType) and n != 'BaseType':
+            clsaaes_cache[n] = t
+            clsaaes_cache[f"{__name__}.{n}"] = t
 
 class BaseType:
+    def __init__(self, **option):
+        '''option -> dict'''
+        self.__dict__['option'] = option
+
+    def __getattr__(self, item):
+        return self.option.get(item)
+
+    def __setattr__(self, key, value):
+        raise NotImplementedError()
 
     def stringify(self, value):
         raise NotImplementedError()
@@ -25,8 +77,18 @@ class BaseType:
 class Int(BaseType):
 
     def stringify(self, value):
+        val = int(value)
+        # max = self.option.get('max')
+        # max = self.get('max')
+        max = self.max
+        if max and val > max:
+            raise ValueError(f'{val} Too Big')
+        min = self.option.get('min')
+        if min and val < min:
+            raise ValueError(f'{val} Too Small')
+
         # 要将int序列化，即要先是int类型,因为是存储的数据类型是text，所以只能是str
-        return str(int(value))
+        return str(val)
 
     def destringify(self, value):
         return int(value)
@@ -35,7 +97,13 @@ class Int(BaseType):
 class IP(BaseType):
     def stringify(self, value):
         '''传入ip字符串或者数字,转换错误不要给默认值，在外面处理'''
-        return str(ipaddress.ip_address(value))
+        val = str(ipaddress.ip_address(value))
+        if not val.startswith(self.prefix):
+            raise ValueError(f'{val} not startswith to {self.prefix}')
+        return val
 
     def destringify(self, value):
         return value
+
+#加载类到字典
+inject()
